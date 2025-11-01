766c736b675b9e01e9de09cdd2a35bec0739fae4
Stefanhg <48594001+Stefanhg@users.noreply.github.com>
2025-11-01 23:07:50 +0100

    Fixed spaces in code

--- led_step_test/led_step_test.ino
+++ led_step_test/led_step_test.ino
@@ -14,15 +14,15 @@ enum SelMode : uint8_t { SEL_NONE,
 
 enum EcoMode : uint8_t { MODE_ECO,
                          MODE_NORMAL,
-                         MODE_BOOST};
+                         MODE_BOOST };
 
 struct BatEcoDigitStruct {
   uint8_t bat_indicator;
-  EcoMode eco;   // which selector to assert
+  EcoMode eco;  // which selector to assert
   uint8_t digit;
 };
 
-BatEcoDigitStruct bat_eco_digit = {25, MODE_NORMAL, 8}; 
+BatEcoDigitStruct bat_eco_digit = { 25, MODE_NORMAL, 8 };
 const uint8_t BAT_ECO_DISP_ADDR = 0x3A;
 const uint8_t TRIP_CNT_ADDR = 0x39;
 const uint8_t ODOMETER_ADDR = 0x3A;
@@ -99,26 +99,26 @@ uint8_t number_to_saa1064_digit(uint8_t number) {
     case 9:
       return 111;
     default:
-      return 0; // Blank for out of range
+      return 0;  // Blank for out of range
   }
-} 
+}
 
 void update_eco_bat_disp() {
-  /*
+/*
   Updates the battery/eco display based on the current bat_eco_digit struct values
   */
 
-  // 16 levels of battery
-  // at 100%, ALL on
-  // AT 0%, first LED is on
-  #define PERC_TO_LEVEL 6.25 // 100/16
+// 16 levels of battery
+// at 100%, ALL on
+// AT 0%, first LED is on
+#define PERC_TO_LEVEL 6.25  // 100/16
 
   // DISP1 controls last digit
   // DISP2, DISP3 controls battery indicator
   // DISP2 controls UPPER part, DISP3 lower
   // DISP4 bit 0,1,2 controls ECO
   uint8_t numval = number_to_saa1064_digit(bat_eco_digit.digit);
-  uint16_t setval = 1<<(int)(bat_eco_digit.bat_indicator / PERC_TO_LEVEL);
+  uint16_t setval = 1 << (int)(bat_eco_digit.bat_indicator / PERC_TO_LEVEL);
 
 
   // Split setval into upper and lower parts
@@ -144,9 +144,9 @@ void update_eco_bat_disp() {
       upper_val |= (1 << i) - 1;
     }
   }
-  
+
   setSelector(SEL_B);
-  writeDigits(BAT_ECO_DISP_ADDR, numval, upper_val, lower_val, 1<<(int)bat_eco_digit.eco);
+  writeDigits(BAT_ECO_DISP_ADDR, numval, upper_val, lower_val, 1 << (int)bat_eco_digit.eco);
 }
 
 void set_odo_meter(uint32_t odoval) {
@@ -154,7 +154,7 @@ void set_odo_meter(uint32_t odoval) {
   :param odoval: value from 0 to 99999
   */
 
-  // Set 
+  // Set
   // 4 digits, last digit is separate display
   // seperate digit is the LSB
   uint8_t tens = odoval % 10;
@@ -166,25 +166,24 @@ void set_odo_meter(uint32_t odoval) {
 
   // LSB
   bat_eco_digit.digit = tens;
-  
+
 
 
   setSelector(SEL_A);
-  writeDigits(ODOMETER_ADDR, 
-    number_to_saa1064_digit(msb), 
-    number_to_saa1064_digit(ten_thousands), 
-    number_to_saa1064_digit(thousands), 
-    number_to_saa1064_digit(hundreds) // First digit left side 
+  writeDigits(ODOMETER_ADDR,
+              number_to_saa1064_digit(msb),
+              number_to_saa1064_digit(ten_thousands),
+              number_to_saa1064_digit(thousands),
+              number_to_saa1064_digit(hundreds)  // First digit left side
   );
   update_eco_bat_disp();
-
 }
 
 void set_trip_counter(uint16_t tripval) {
   /*
   :param tripval: value from 0 to 9999
   */
-  uint8_t d1 = number_to_saa1064_digit(tripval % 10); // Last digit
+  uint8_t d1 = number_to_saa1064_digit(tripval % 10);  // Last digit
   uint8_t d2 = number_to_saa1064_digit((tripval / 10) % 10);
   uint8_t d3 = number_to_saa1064_digit((tripval / 100) % 10);
   uint8_t d4 = number_to_saa1064_digit((tripval / 1000) % 10);
@@ -211,7 +210,7 @@ void set_speed(uint8_t speedval) {
   uint8_t val64_80 = (uint8_t)((led_full_range_val >> 32) & 0xFF);
   uint8_t val80_96 = (uint8_t)((led_full_range_val >> 40) & 0xFF);
   uint8_t val96_100 = (uint8_t)((led_full_range_val >> 48) & 0x7);  // Only get the last 3 bits
-  
+
   // Write to lower IC
   // d1 of IC = 0-16, d2=32-48, d3=16-32, d4=48-64
   setSelector(SEL_NONE);
@@ -220,7 +219,7 @@ void set_speed(uint8_t speedval) {
   // Write to upper IC
   // d1=64-80, d2=96-100, d3=80-96, d4=unused
   // Selector already set to None
-  writeDigits(SPEEDOMETER_UPPER_ADDR, val64_80, val96_100, val80_96, 0);  
+  writeDigits(SPEEDOMETER_UPPER_ADDR, val64_80, val96_100, val80_96, 0);
 }
 
 
@@ -251,40 +250,40 @@ void setup() {
 void loop() {
 
 
-    //S - Speed
-    //E - ECO
-    //T - Trip
-    //B - Battery
-    //O - Odometer
+  //S - Speed
+  //E - ECO
+  //T - Trip
+  //B - Battery
+  //O - Odometer
 
-    // Read serial and set battery level to value.
-    if (Serial.available() > 0) {
-      String input = Serial.readStringUntil('\n');
-      String cmd = input.substring(0,1);
-      uint32_t val = input.substring(1).toInt();
+  // Read serial and set battery level to value.
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    String cmd = input.substring(0, 1);
+    uint32_t val = input.substring(1).toInt();
 
-      if(cmd == "S") {
-        if (val >= 0 && val <= 100) {
-          set_speed((uint8_t)val);
-        }
-      } else if (cmd == "E") {
-        if (val >= 0 && val <= 2) {
-          bat_eco_digit.eco = (EcoMode)val;
-          update_eco_bat_disp();
-        }
-      } else if (cmd == "T") {
-        if (val >= 0 && val <= 9999) {
-          set_trip_counter((uint16_t)val);
-        }
-      } else if (cmd == "B") {
-        if (val >= 0 && val <= 100) {
-          bat_eco_digit.bat_indicator = (uint8_t)val;
-          update_eco_bat_disp();
-        }
-      } else if (cmd == "O") {
-        if (val >= 0 && val <= 99999) {
-          set_odo_meter((uint32_t)val);
-        }
+    if (cmd == "S") {
+      if (val >= 0 && val <= 100) {
+        set_speed((uint8_t)val);
+      }
+    } else if (cmd == "E") {
+      if (val >= 0 && val <= 2) {
+        bat_eco_digit.eco = (EcoMode)val;
+        update_eco_bat_disp();
+      }
+    } else if (cmd == "T") {
+      if (val >= 0 && val <= 9999) {
+        set_trip_counter((uint16_t)val);
+      }
+    } else if (cmd == "B") {
+      if (val >= 0 && val <= 100) {
+        bat_eco_digit.bat_indicator = (uint8_t)val;
+        update_eco_bat_disp();
+      }
+    } else if (cmd == "O") {
+      if (val >= 0 && val <= 99999) {
+        set_odo_meter((uint32_t)val);
       }
     }
+  }
 }
